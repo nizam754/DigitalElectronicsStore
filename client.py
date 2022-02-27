3e5108a59f8ec01989ede940d2532a66a4ecd7d7
nizam754 <m.nizam754@gmail.com>
2022-02-28 00:22:29 +0600

    client file need to complete

---
+++ client.py
@@ -0,0 +1,13 @@
+from mobile_tech import Mobile
+from laptop_tech import Laptop
+from tech_product import Tech
+from sales_person import SalesPerson
+from datetime import date
+
+phone_1 = Mobile('iphone_11', 60000, 500, 'Black', '1920-1080', 10)
+phone_2 = Mobile('iphone_12', 70000, 550, 'Silver', '1920-1080', 12)
+phone_3 = Mobile('iphone_13', 80000, 580, 'Project Red', '1920-1080', 13)
+
+laptop_1 = Laptop('Asus g14', 130000, 1.6, 'Moonlight Silver', 16, 'Ryzen 480H', 1000)
+laptop_2 = Laptop('Macbook Pro 13', 130000, 1.7, 'Dark Grey', 8, 'Intel Core i5', 256)
+laptop_3 = Laptop('Dell XPS 13', 140000, 1.4, 'White', 16, 'Intel Core i7', 512)
